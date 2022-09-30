750205d31b3e1c2b139ff6a57c00336ad0980a2e
Borja Montseny <borja.montseny@gmail.com>
2022-09-30 19:47:45 +0200

    .

--- src/Ex01.java
+++ src/Ex01.java
@@ -4,6 +4,13 @@ public class Ex01 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		/*
+		 * Escriu un programa que demani 2 números i digui si els dos són parells, o un
+		 * o els dos són senars.
+		 */
+
+		
+		
 	}
 
 }
---
+++ src/Ex03.java
@@ -0,0 +1,14 @@
+
+public class Ex03 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * Tenint en compte els valors de a, b, c i x, indica si les següents
+		 * expressions s'avaluen com a CERT o FALS:
+		 */
+
+	}
+
+}
