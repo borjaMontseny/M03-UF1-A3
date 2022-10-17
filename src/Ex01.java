6641331aedfa37f9327caf8d226214f506616710
Borja Montseny <borja.montseny@gmail.com>
2022-10-17 13:42:04 +0200

    .

--- src/Ex01.java
+++ src/Ex01.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class Ex01 {
 
@@ -9,8 +10,27 @@ public class Ex01 {
 		 * o els dos són senars.
 		 */
 
-		
-		
+		System.out.print("Introdueix valor A: ");
+		Scanner sc = new Scanner(System.in);
+		int valorA = sc.nextInt();
+
+		System.out.print("Introdueix el valor B: ");
+		int valorB = sc.nextInt();
+		sc.close();
+
+		if ((valorA % 2 == 0) && (valorB % 2 == 0)) {
+			System.out.println("Els dos són parells");
+
+		} else if ((valorA % 2 != 0) && (valorB % 2 != 0)) {
+			System.out.println("Els dos són senars");
+
+		} else if ((valorA % 2 != 0) || (valorB % 2 != 0)) {
+			System.out.println("Un dels dos és senar");
+
+		} else {
+			System.out.println("Cap dels dos són parells o senars a la vegada");
+		}
+
 	}
 
 }
--- src/Ex02.java
+++ src/Ex02.java
@@ -3,6 +3,13 @@ public class Ex02 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		/*
+		 * Escriu un programa que demani 1 paraula, i digui si està entre alguna de les
+		 * tres següents: “Arxiu”, “Buscar”, “Sortir”, en cas que no hi estigui, indicar
+		 * que hi ha un error.
+		 */
+		
+		
 
 	}
 
